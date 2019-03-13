cb6483a8395d252493a7933724d4256373a83dd1
Asia Carter <asiamcarter@gmail.com>
2019-03-13 15:02:20 -0500

    setting up project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_StudentExercises2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
